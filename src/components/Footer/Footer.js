d9d80ea63d852d6ec1815f1e9c0c83b7d1bed085
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2024-03-20 17:20:06 +0530

    created header

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <h1 className='footer'>Footer</h1>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img className="logo" src={assets.logo} alt="logo" />
+      <div className="nav">
+        <ul className="nav-Item">
+          <li className="nav-list">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-list">
+            <a href="/">Menu</a>
+          </li>
+          <li className="nav-list">
+            <a href="/">Mobile-App</a>
+          </li>
+          <li className="nav-list">
+            <a href="/">Contact Us</a>
+          </li>
+        </ul>
+        <div className="navbar-right">
+          <img src={assets.search_icon} alt="" />
+          <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+          </div>
+          <button>Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
