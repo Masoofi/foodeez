fb845b4524d5b3d6bb105615c74ed098cbc6c043
Muhammed Abubacker Sufi <sufiabubacker@gmail.com>
2024-03-28 11:04:40 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home"
 import Cart from './Pages/Cart/Cart';
 import PlaceOrder from './Pages/PlaceOrders/PlaceOrder';
- 
+
 
 function App() {
   return (
